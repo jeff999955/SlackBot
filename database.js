4e9ff33acefa3baed2246ad0b4d01691beeb6234
Jeff <jeff999955@gmail.com>
2022-06-24 23:12:35 +0800

    Change to asynchronous function in database

--- database.js
+++ database.js
@@ -2,12 +2,10 @@ const Problem = require("./model/problem");
 const mongoose = require("mongoose");
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: 4000 });
+require("dotenv").config();
+const wss = new WebSocket.Server({ port: process.env.PORT });
 
-MONGO_URL =
-  "mongodb+srv://nl:nlnl8787@cluster0.hnxxxp6.mongodb.net/?retryWrites=true&w=majority";
-
-mongoose.connect(MONGO_URL, {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -30,30 +28,34 @@ db.once("open", () => {
       sendData(["status", s]);
     };
 
-    const findProblem = (callback) => {
-      Problem.find()
-        .sort({ "difficulty.level": -1 })
-        .limit(10)
-        .exec((err, res) => {
-          if (err) throw err;
-          callback();
-        });
+    const findProblem = async (condition) => {
+      return await Problem.find(condition);
     };
 
-    ws.onmessage = ({ data }) => {
+    ws.onmessage = async ({ data }) => {
       // console.log(data);
       const [task, payload] = JSON.parse(data);
 
-      console.log(payload[0]);
       switch (task) {
         case "insert": {
           // payload: List of problems
-          Problem.insertMany(payload)
-            .then(sendData(["success", "success"]))
-            .catch((e) => {
-              sendData(["error", e]);
-              console.log(e);
-            });
+          try {
+            await Problem.insertMany(payload);
+            sendStatus("success");
+          } catch (err) {
+            sendStatus("fail");
+            console.error(err);
+          }
+          break;
+        }
+        case "find": {
+          // payload: Problem
+          try {
+            sendData(["success", await findProblem(payload)]);
+          } catch (err) {
+            sendStatus("fail");
+            console.error(err);
+          }
           break;
         }
         default:
